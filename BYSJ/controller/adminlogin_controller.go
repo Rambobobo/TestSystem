94741d973f34649b4eede292547504aff422bb28
Lanjirong <578904035@qq.com>
2023-03-02 16:19:45 +0800

    修改管理员登录service和controller层

--- BYSJ/controller/adminlogin_controller.go
+++ BYSJ/controller/adminlogin_controller.go
@@ -6,24 +6,40 @@ import (
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/mvc"
 )
-
+/*
+管理员控制器
+*/
+//将发送的请求的字段映射为指定字段
+type AdminLogin struct {
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
 type AdminController struct{
 	ctx iris.Context
+	Serive service.AdminService
 }
 
-func (c *AdminController) PostLogin(ctx iris.Context) mvc.Result{
-	iris.New().Logger().Info("admin login")
-	username :=ctx.FormValue("username")
-	password := ctx.FormValue("password")
-	admin,err:=service.AdminLogin(username,password)
-	if err!=nil{
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.WriteString(err.Error())
-		return
+func (ac *AdminController) PostLogin(ctx iris.Context) mvc.Result{
+	var adminlogin AdminLogin
+	ac.ctx.ReadJSON(&adminlogin)
+
+	if adminlogin.UserName ==""||adminlogin.Password==""{
+		return mvc.Response{
+			Object: map[string]interface{}{
+				"status":"0",
+				"success":"登录失败",
+				"message":"用户名为空",
+			},
+		}
 	}
-	if admin==nil {
-		ctx.ViewData("Error","用户名密码错误")
-		return
-		
+	admin,exist :=ac.Serive.GetByAdminNameAndPassword(adminlogin.UserName,adminlogin.Password)
+	if !exist{
+		return mvc.Response{
+			Object: map[string]interface{}{
+				"status":"1",
+				"success":"登录失败",
+				"message":"管理员不存在",
+			},
+		}
 	}
 }
